327b1ec778036e465bea1c9989e6695a9f4425ee
oht1221 <oht1221@gmail.com>
2017-11-07 16:16:28 +0900

    update assign function_2

--- scheduler.py
+++ scheduler.py
@@ -135,23 +135,20 @@ def assign(CNCs, job_pool):  #CNC에 job들을 분배하는 함수
             cnc.enQ(assignment)
             print("a new job(%s) asggined to CNC #(%s)!\n--------------------------"
                   "--------------------------------------------------" % (assignment.getNumber(), cnc.getNumber()))
+
     return last_assigned_cnc'''
+
+    avgTime = sum(list(job.getTime() for job in job_pool)) / len(job_pool)
 
     normPool = []
     hexPool = []
+    splitPool(job_pool,normPool,hexPool)
+    normPool = permutations(normPool,len(normPool))
+    hexPool = permutations(hexPool,len(hexPool))
+    normCNCs = list(filter(lambda x : x.getShape() == 0, CNCs))
+    hexCNCs = list(filter(lambda x : x.getShape() == 1, CNCs))
 
-    for i,assignment in enumerate(job_pool):
-        if assignment.getType() == 0:
-            normPool.append(assignment)
-            del job_pool[i]
-
-        elif assignment.getType() == 1:
-            hexPool.append(assignment)
-            del job_pool[i]
-
-    normPool = permutations(normPool)
-    hexPool = permutations(hexPool)
-
+    
 
 def newJobs():
     return np.random.choice([0,1], 1, p = [0.9995, 0.0005])
@@ -172,3 +169,15 @@ def update(CNCs, unitTime):
             if(i == len(job.getSeries()) - 1):  ##마지막 콤포넌트까지 모두 done이면
                 if(job.ifAllDone()): #job의 함수를 통해 한번더 검사하고
                     c.deQ() #job을 jobQ에서 뺀다.
+def splitPool(job_pool, normPool, hexPool):
+
+    for i,assignment in enumerate(job_pool):
+        if assignment.getType() == 0:
+            normPool.append(assignment)
+            del job_pool[i]
+
+        elif assignment.getType() == 1:
+            hexPool.append(assignment)
+            del job_pool[i]
+
+    return 0
