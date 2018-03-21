909d1114e5ea56039f65441878f1d4a6b0e3631a
oht1221 <oht1221@gmail.com>
2018-03-22 02:41:33 +0900

    revising schedule display

---
+++ main.py
@@ -0,0 +1,26 @@
+import time
+import scheduler
+import genetic
+import cnc
+import AccessDB
+from collections import deque
+
+if __name__ == "__main__":
+    CNCs = []
+    JOB_POOL = deque()
+    READY_POOL = deque()
+    IN_PROGRESS = deque()
+
+    TOTAL_NUMBER_OF_THE_POOL = scheduler.make_job_pool(JOB_POOL)
+    scheduler.read_CNCs('./hansun2.xlsx', CNCs)
+
+
+    machines = {}
+    for cnc in CNCs:
+        machines[float(cnc.getNumber())] = list()
+    standard = input("schedule starts on : ")
+    standard = (lambda x: int(time.time()) if (x == 'now') else time.mktime(
+        (int(x[0:4]), int(x[4:6]), int(x[6:8]), 12, 0, 0, 0, 0, 0)))(standard)
+    standard = int(standard)
+    genetic.initialize_population(JOB_POOL)
+    genetic.start(machines, standard, CNCs, TOTAL_NUMBER_OF_THE_POOL)
